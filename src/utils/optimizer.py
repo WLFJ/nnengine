6bc6df43eaec2a1dc58a9eda76fcd68101fc3b6a
Amadeus <13202127055@163.com>
2022-12-01 15:30:47 +0800

    [代码] 更多 Optimizer

--- src/utils/optimizer.py
+++ src/utils/optimizer.py
@@ -3,38 +3,37 @@ import numpy as np
 
 
 class Optimizer:
-    def __init__(self, parameters):
+    def __init__(self, parameters, lr):
         self.parameters = parameters
+        self.lr = lr
 
     def step(self, zero=True):
         raise NotImplemented
-
-
-class SGD(Optimizer):
-
-    def __init__(self, parameters, lr=0.1):
-        self.parameters = parameters
-        self.alpha = lr
 
     def zero(self):
         for p in self.parameters:
             p.grad *= 0
 
+
+class SGD(Optimizer):
+
+    def __init__(self, parameters, lr=0.1):
+        super(SGD, self).__init__(parameters, lr)
+
     def step(self, zero=True):
 
         for p in self.parameters:
 
-            p.data -= p.grad * self.alpha
+            p.data -= p.grad * self.lr
 
-            if (zero):
+            if zero:
                 p.grad *= 0
 
 
 class Adam(Optimizer):
 
     def __init__(self, parameters, lr=0.001, beta=(0.9, 0.999), eps=1e-8):
-        self.parameters = parameters
-        self.alpha = lr
+        super(Adam, self).__init__(parameters, lr)
         self.beta = beta
         self.eps = eps
 
@@ -45,10 +44,6 @@ class Adam(Optimizer):
         for p in self.parameters:
             self.m.append(Tensor(np.zeros_like(p.data)))
             self.v.append(Tensor(np.zeros_like(p.data)))
-
-    def zero(self):
-        for p in self.parameters:
-            p.grad *= 0
 
     def step(self, zero=True):
 
@@ -62,7 +57,52 @@ class Adam(Optimizer):
             m_hat = m.data / (1 - self.beta[0] ** self.t)
             v_hat = v.data / (1 - self.beta[1] ** self.t)
 
-            p.data -= self.alpha * m_hat / (np.sqrt(v_hat) + self.eps)
+            p.data -= self.lr * m_hat / (np.sqrt(v_hat) + self.eps)
 
-            if (zero):
+            if zero:
+                p.grad *= 0
+
+
+class AdaGrad(Optimizer):
+
+    def __init__(self, parameters, lr=0.001, eps=1e-8):
+        super(AdaGrad, self).__init__(parameters, lr)
+        self.eps = eps
+
+        self.m = list()
+
+        for p in self.parameters:
+            self.m.append(Tensor(np.zeros_like(p.data)))
+
+    def step(self, zero=True):
+
+        for p, m in zip(self.parameters, self.m):
+
+            m.data += p.grad * p.grad
+            p.data -= self.lr * p.grad / (np.sqrt(m.data) + self.eps)
+
+            if zero:
+                p.grad *= 0
+
+
+class RMSprop(Optimizer):
+
+    def __init__(self, parameters, lr=0.001, beta=0.9, eps=1e-8):
+        super(RMSprop, self).__init__(parameters, lr)
+        self.beta = beta
+        self.eps = eps
+
+        self.m = list()
+
+        for p in self.parameters:
+            self.m.append(Tensor(np.zeros_like(p.data)))
+
+    def step(self, zero=True):
+
+        for p, m in zip(self.parameters, self.m):
+
+            m.data = self.beta * m.data + (1 - self.beta) * (p.grad * p.grad)
+            p.data -= self.lr * p.grad / (np.sqrt(m.data) + self.eps)
+
+            if zero:
                 p.grad *= 0
