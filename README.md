b5c8ce7f89fc30029df1712b1a37cc31d57b3332
Amadeus <13202127055@163.com>
2022-12-01 14:50:23 +0800

    [代码] 添加 Scheduler

--- README.md
+++ README.md
@@ -7,15 +7,15 @@ BUAA 2022 硕士高等软工课程设计
 ```
 .
 ├── src
-│   ├── core                     张量、模型等核心接口
-│   │   ├── nn.py
-│   │   └── tensor.py
-│   ├── data                     数据
-│   │   └── dataloader.py
-│   └── utils                    训练、测试工具
-│       ├── evaluator.py         评估器
-│       ├── optimizer.py         优化器
-│       └── trainer.py           训练器
+│   ├── core                     张量、模型等核心接口
+│   │   ├── nn.py
+│   │   └── tensor.py
+│   ├── data                     数据
+│   │   └── dataloader.py
+│   └── utils                    训练、测试工具
+│       ├── evaluator.py         评估器
+│       ├── optimizer.py         优化器
+│       └── trainer.py           训练器
 ├── test.py                            测试
 └── usr                                自订模型、数据集
     └── mnist_dataset.py
---
+++ src/utils/scheduler.py
@@ -0,0 +1,100 @@
+from optimizer import Optimizer
+import numpy as np
+
+
+class Scheduler:
+
+    def __init__(self, optimizer):
+        self.optimizer = optimizer
+        pass
+
+
+class MultiStepLR(Scheduler):
+
+    def __init__(self, optimizer: Optimizer, milestones: [int], lr_decay: float = 0.1):
+        super(MultiStepLR, self).__init__(optimizer)
+        self.milestones = milestones
+        self.lr_decay = lr_decay
+        self.epoch = 0
+
+    def step(self):
+        self.epoch += 1
+        if self.epoch in self.milestones:
+            self.optimizer.lr *= self.lr_decay
+
+
+class StepLR(Scheduler):
+
+    def __init__(self, optimizer: Optimizer, step_size: int, lr_decay: float = 0.1):
+        super(StepLR, self).__init__(optimizer)
+        self.step_size = step_size
+        self.lr_decay = lr_decay
+        self.epoch = 0
+
+    def step(self):
+        self.epoch += 1
+        if self.epoch % self.step_size == 0:
+            self.optimizer.lr *= self.lr_decay
+
+
+class Exponential(Scheduler):
+
+    def __init__(self, optimizer: Optimizer, lr_decay: float):
+        super(Exponential, self).__init__(optimizer)
+        self.lr_decay = lr_decay
+        self.epoch = 0
+
+    def step(self):
+        self.epoch += 1
+        self.optimizer.lr *= self.lr_decay
+
+
+class Cosine(Scheduler):
+
+    def __init__(self, optimizer: Optimizer, T_max: int, eta_min: float = 0):
+        super(Cosine, self).__init__(optimizer)
+        self.T_max = T_max
+        self.eta_min = eta_min
+        self.epoch = 0
+
+    def step(self):
+        self.epoch += 1
+        self.optimizer.lr = self.eta_min + (self.optimizer.lr - self.eta_min) * (
+                1 + np.cos(np.pi * self.epoch / self.T_max)) / 2
+
+
+class LambdaLR(Scheduler):
+
+    def __init__(self, optimizer: Optimizer, lr_lambda: callable):
+        super(LambdaLR, self).__init__(optimizer)
+        self.lr_lambda = lr_lambda
+        self.epoch = 0
+
+    def step(self):
+        self.epoch += 1
+        self.optimizer.lr = self.lr_lambda(self.epoch)
+
+
+class ReduceLROnPlateau(Scheduler):
+
+    def __init__(self, optimizer: Optimizer, factor: float, patience: int, verbose: bool = False):
+        super(ReduceLROnPlateau, self).__init__(optimizer)
+        self.factor = factor
+        self.patience = patience
+        self.verbose = verbose
+        self.epoch = 0
+        self.best = float('inf')
+        self.wait = 0
+
+    def step(self, loss):
+        self.epoch += 1
+        if loss < self.best:
+            self.best = loss
+            self.wait = 0
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.optimizer.lr *= self.factor
+                self.wait = 0
+                if self.verbose:
+                    print(f'Epoch {self.epoch}: ReduceLROnPlateau reducing learning rate to {self.optimizer.lr}.')
