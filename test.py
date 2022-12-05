7df42234174d7d94c88c24f7d714ccedc5063bd9
Amadeus <13202127055@163.com>
2022-12-05 15:46:10 +0800

    [测试] Linear 单元测试示例

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from toynn.core.nn import Model, Sequential, Linear, Conv2d
 from toynn.data import split
 from toynn.utils import mseLoss, maeLoss, crossEntropyLoss
 from toynn.core.nn import Linear
+from toynn.utils.scheduler import MultiStepLR, StepLR, Exponential, Cosine
 
 import numpy as np
 
@@ -19,11 +20,13 @@ train_dataset, test_dataset = split(dataset, 0.8)
 
 opt = SGD(parameters=m.get_parameters(), lr=0.01)
 
+sch = MultiStepLR(opt, [10, 20, 30, 40, 50, 60, 70, 80, 90])
+
 trainer = Trainer(m, opt, mseLoss, {
     "epochs": 100,
     "batch_size": 10,
     "shuffle": True
-})
+}, sch)
 
 loss = trainer.train(train_dataset, test_dataset)
 
